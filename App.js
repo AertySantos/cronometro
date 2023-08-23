18b38d5511ddd442e4bb3abe673e8b35cf5de39d
aertyps <aertyps@hotmail.com>
2023-08-23 19:46:53 -0300

    fim

--- App.js
+++ App.js
@@ -8,6 +8,41 @@ import {
 } from 'react-native';
 
 class App extends Component{
+
+  constructor(props) {
+    
+    super(props);
+    this.state = {
+      numero: 0,
+      botao:'Vai'
+    };
+
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+    this.timer = null;
+    
+  }
+
+  vai() {
+    
+    if (this.timer != null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({ botao: 'Vai' });
+    } else {
+      this.timer = setInterval(() => {
+      this.setState({numero: this.state.numero + 0.1})
+      }, 100);
+      this.setState({botao: 'Parar'});
+    }
+  }
+
+  limpar() {
+    this.setState({ numero: 0 });
+    clearInterval(this.timer);
+    this.timer = null;
+    this.setState({ botao: 'Vai' })
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -15,13 +50,13 @@ class App extends Component{
           source={require('./src/cronometro.png')}
           style={styles.cronometro}
         />
-        <Text style={styles.timer}>0.0</Text>
+        <Text style={styles.timer}>{ this.state.numero.toFixed(1)}</Text>
         <View style={styles.btnArea}>
-          <TouchableOpacity style={styles.btn}>
-            <Text style={styles.btnText}>Vai</Text>
+          <TouchableOpacity style={styles.btn} onPress= {this.vai}>
+            <Text style={styles.btnText}>{ this.state.botao}</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.btn}>
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
             <Text style={styles.btnText}>Limpar</Text>
           </TouchableOpacity>
 
@@ -39,7 +74,28 @@ const styles = StyleSheet.create({
   },
   timer: {
     marginTop: -160,
-    
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight:'bold'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 70,
+    height:40
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius:9
+  },
+  btnText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color:'#00aeef'
   }
 });
 
